c6b4eac111ca56d818c9e156100b22c4b6199fd4
Betu <bkay210@gmail.com>
2018-11-18 22:21:50 -0500

    first commit

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var bird;
+function setup() {
+createCanvas(400, 600);
+bird = new Bird();
+}
+
+function draw() {
+background(0);
+bird.show();
+}
+
+function keyPressed() {
+  if(key == '') {
+    
+  }
+}
